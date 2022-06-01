5b9a9f1e0fc27d7af891bd8352d2dca62135c60e
Valerii GANTCEV <4vpost@gmail.com>
2022-06-01 15:13:09 +0400

    Семинар 5. Задание 3. Показать числа Фибоначчи меньше заданного числа N

---
+++ S5Z3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Семинар 5. Задание 3. Показать числа Фибоначчи меньше заданного числа N
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()?? "");
+int firstValue = 0;
+int secondValue = 1;
+int result = 0;
+Console.Write($"{firstValue} {secondValue} ");
+while(firstValue + secondValue < N)
+{
+result = firstValue + secondValue;
+Console.Write(result + " ");
+firstValue = secondValue;
+secondValue = result;
+}
