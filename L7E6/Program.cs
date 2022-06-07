ead4974a052b074e97b316e268a7d6176a9e8607
Valerii GANTCEV <4vpost@gmail.com>
2022-06-07 23:57:52 +0400

    // Лекция 7. Рекурсия (7.6.22) Перебор слов

---
+++ L7E6/Program.cs
@@ -0,0 +1,16 @@
+﻿// Лекция 7. Рекурсия (7.6.22) Перебор слов
+Console.Clear();
+int n = 1;
+void FindWords (string alphabit, char [] word, int len = 0)
+{
+    if (len==word.Length)
+    {
+        Console.WriteLine($"{n++} {new String (word)}"); return;
+    }
+    for (int i=0; i<alphabit.Length; i++)
+    {
+        word[len]=alphabit[i];
+        FindWords(alphabit,word,len+1);
+    }
+}
+FindWords("аисв", new char[5]);
