1a23d2db0be80369b74823eec335efc45167e868
Valerii GANTCEV <4vpost@gmail.com>
2022-06-08 00:42:50 +0400

    // Лекция 7. Рекурсия (7.6.22) Дата создания директории (папки) и состав файлов папки

---
+++ L7E7/Program.cs
@@ -0,0 +1,10 @@
+﻿// Лекция 7. Рекурсия (7.6.22) Дата создания директории (папки) и состав файлов папки
+Console.Clear();
+string path = "C:/Users/4vpos/C_SH2022_DZ/D1Z1";
+DirectoryInfo di = new DirectoryInfo(path);
+System.Console.WriteLine(di.CreationTime);
+FileInfo [] fi = di.GetFiles();
+for (int i = 0; i < fi.Length; i++)
+{
+    System.Console.WriteLine(fi[i].Name);
+}
