6ba2a52fb0470cd723dffed33232c5d2fae344c4
Valerii GANTCEV <4vpost@gmail.com>
2022-06-09 12:33:48 +0400

    // Найти сумму цифр числа Семинар 10.

---
+++ S10Z1/Program.cs
@@ -0,0 +1,7 @@
+﻿// Найти сумму цифр числа
+Console.Clear ();
+uint SumDigits(uint n) => n<10 ? n : n%10 + SumDigits(n/10);
+Console.Write("Enter your number: ");
+uint n = uint.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine($"Sum of digits {SumDigits(n)}");
+
