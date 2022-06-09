5d076477e61415d0a300c0d5b2fa975266e0394b
Valerii GANTCEV <4vpost@gmail.com>
2022-06-10 01:41:20 +0400

    Задание 1. Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы

--- D5Z1/Program.cs
+++ D5Z1/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Задание 1. Найти точку пересечения двух прямых заданных уравнением 
 // y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
 Console.Clear();
-string X(int k1, int k2, int b1, int b2)
+string Cross (int k1, int k2, int b1, int b2)
 {
     string result = string.Empty;
     int x = 0;
@@ -9,6 +9,7 @@ string X(int k1, int k2, int b1, int b2)
     x = (b2 - b1) / (k1 - k2);
     y = (k1 * (b2 - b1) / (k1 - k2)) + b1;
     result = $"({x}, {y})";
-    return result;
+  return result;
 }
-Console.WriteLine(X(10, 15, 40, 50));
+Console.WriteLine(Cross(30, 17, 40, 50));
+
