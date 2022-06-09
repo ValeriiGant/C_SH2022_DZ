a5800df7cd287b5ec48907edef080bfe101d1371
Valerii GANTCEV <4vpost@gmail.com>
2022-06-10 02:17:56 +0400

    // Задание 3. Написать программу копирования массива

---
+++ D5Z3/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задание 3. Написать программу копирования массива 
+Console.Clear();
+Console.WriteLine("Программа копирования массива:");
+
+void CreateArray(int[] createArray,int min,int max)
+{
+	for (int i = 0; i < createArray.Length; i++)
+	{
+		createArray[i] = new Random().Next(min, max);
+	}
+}
+
+void PrintArray(int[] printArray)
+{
+	Console.Write(printArray[0]);
+	for (int i = 1; i < printArray.Length; i++)
+	{
+		Console.Write($", {printArray[i]}");
+	}
+	Console.WriteLine();
+}
+
+Console.Write("Введите длинну массива: ");
+int length = int.Parse(Console.ReadLine());
+
+int[] array = new int[length];
+CreateArray(array, -100, 100);
+Console.WriteLine("Первоначальный массив: ");
+PrintArray(array);
+
+int[] newArray = new int[array.Length];
+for (int i = 0; i < array.Length; i++)
+{
+	newArray[i] = array[i];
+}
+Console.WriteLine("Копия массива: ");
+PrintArray(newArray);
