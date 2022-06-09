e1db5cee504ea9979af13f1c1a16d828cb494738
Valerii GANTCEV <4vpost@gmail.com>
2022-06-09 14:06:44 +0400

    Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих.Первые два элемента последовательности задаются пользователем

---
+++ S10Z3/Program.cs
@@ -0,0 +1,39 @@
+﻿// Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих. 
+//Первые два элемента последовательности задаются пользователем
+Console.Clear ();
+int SumTwoPrevious(int number1, int number2, int n)
+{
+    if (n == 1) return number1;
+    if (n == 2) return number2;
+    else return SumTwoPrevious(number1, number2, n - 1) + SumTwoPrevious(number1, number2, n - 2);
+
+}
+
+int number1=2;
+int number2=3;
+int n=10;
+for (int i = 1; i <= n; i++)
+{
+    Console.Write(SumTwoPrevious(number1, number2, i) + " ");
+}
+
+//Variant.2
+//void PrintNums(int first, int second, int n)
+//{
+   // if(n==0) return ;
+  //  Console.WriteLine(first + second);
+   // int tmp = first;
+  //  first = second;
+  //  second += tmp;
+  //  PrintNums(first,second,n-1);
+//}
+
+//Console.Write("Введите число N: ");
+//int N = int.Parse(Console.ReadLine()?? "");
+//Console.Write("Введите первый элемент : ");
+//int first = int.Parse(Console.ReadLine()?? "");
+//Console.Write("Введите второй элемент : ");
+//int second = int.Parse(Console.ReadLine()?? "");
+
+//PrintNums(first,second,N);
+
