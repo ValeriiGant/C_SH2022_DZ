af29add8da32a4dbf14b04205b29b7d2305676a3
Valerii GANTCEV <4vpost@gmail.com>
2022-07-03 01:23:49 +0400

    i < matrix.GetLength (0);j< matrix.GetLength (1);

--- L4E1/Program.cs
+++ L4E1/Program.cs
@@ -17,11 +17,11 @@ table[1,2] = "слово";
 //}
 
 int [,] matrix = new int [3,4];
-for (int i=0; i <3; i++)
+for (int i=0; i < matrix.GetLength (0); i++)
 {
-    for (int j=0; j<4; j++)
+    for (int j=0; j< matrix.GetLength (1); j++)
     {
         Console.Write($"{matrix [i,j]}");
     }
-    Console.WriteLine ();
+    Console.WriteLine();
 }
