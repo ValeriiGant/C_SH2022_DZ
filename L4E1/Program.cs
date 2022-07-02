93b460f254251a728f57c6c1512fdcfdd0c7f3f8
Valerii GANTCEV <4vpost@gmail.com>
2022-07-03 01:46:40 +0400

    void PrintArray(int[,] matr) void FillArray (int[,] matr) matr[i,j]=new Random().Next(1,10); int [,] matrix = new int [3,4]; PrintArray (matrix); FillArray(matrix); Console.WriteLine(); PrintArray(matrix);

--- L4E1/Program.cs
+++ L4E1/Program.cs
@@ -16,12 +16,33 @@ table[1,2] = "слово";
     //}
 //}
 
-int [,] matrix = new int [3,4];
-for (int i=0; i < matrix.GetLength (0); i++)
+
+void PrintArray(int[,] matr)
 {
-    for (int j=0; j< matrix.GetLength (1); j++)
+    for (int i=0; i < matr.GetLength (0); i++)
     {
-        Console.Write($"{matrix [i,j]}");
+        for (int j=0; j< matr.GetLength (1); j++)
+        {
+            Console.Write($"{matr [i,j]}");
+        }
+        Console.WriteLine();
     }
-    Console.WriteLine();
+    
 }
+void FillArray (int[,] matr)
+{
+    for (int i=0; i < matr.GetLength (0); i++)
+    {
+        for (int j=0; j< matr.GetLength (1); j++)
+        {
+            matr[i,j]=new Random().Next(1,10); // [1;10)
+           
+        }
+    }
+}
+
+int [,] matrix = new int [3,4];
+PrintArray (matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
