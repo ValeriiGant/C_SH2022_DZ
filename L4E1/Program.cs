f87f79514b7cef9a6cf71f9939490a67cf08ff0e
Valerii GANTCEV <4vpost@gmail.com>
2022-07-03 00:44:06 +0400

    matrix

--- L4E1/Program.cs
+++ L4E1/Program.cs
@@ -7,10 +7,19 @@ string[,] table = new string[2,5]
 
 table[1, 2] = "слово";
 
-for (int rows = 0; rows < 2; rows ++)
+//for (int rows = 0; rows < 2; rows ++)
+//{
+    //for (int columns = 0; columns < 5; columns++)
+    //{
+        //Console.WriteLine($"-{table[rows, columns]}-");
+    //}
+//}
+
+int [,] matrix = new int [3,4];
+for (int i=0; i <3; i++)
 {
-    for (int columns = 0; columns < 5; columns++)
+    for (int j=0; j<4; j++)
     {
-        Console.WriteLine($"-{table[rows, columns]}-");
+        Console.WriteLine ($"{matrix [i,j]}");
     }
 }
