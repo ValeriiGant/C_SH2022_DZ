d4b90864c746c626a603360dfdd4036e9ab4308b
Valerii GANTCEV <4vpost@gmail.com>
2022-07-03 05:42:06 +0400

    // Задание 1. Показать натуральные числа от M до N, N и M заданы

---
+++ D9Z1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задание 1. Показать натуральные числа от M до N, N и M заданы
+Console.Clear();
+string Digits(int m, int n)
+{
+    if(m == n) return m.ToString();
+    return m.ToString() + " " + Digits(m + 1, n);
+}
+
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+Console.Write($"Натуральные числа от М = {m} до N = {n}: " );
+Console.WriteLine(Digits(m,n));
